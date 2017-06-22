6ca7790fa2a6547cd07acd0ff0bee0b3121b3c20
Aslan Vatsaev <aslan.vatsaev@ihu-strasbourg.eu>
2017-06-22 17:03:39 +0200

    perf: change detection strategy OnPush for thread components

--- src/app/components/thread-list/thread-list.component.ts
+++ src/app/components/thread-list/thread-list.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {Thread} from "../../models/thread";
 
 @Component({
   selector: 'app-thread-list',
   templateUrl: './thread-list.component.html',
-  styleUrls: ['./thread-list.component.sass']
+  styleUrls: ['./thread-list.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThreadListComponent implements OnInit {
 
